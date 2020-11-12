1b12797876f53904d72d9cd9be29b4f50be0587f
James Frederix Rolianto <jamesfrederix16@gmail.com>
2020-11-12 11:56:28 +0700

    add navbar and footer

--- ppw_kel_12/settings.py
+++ ppw_kel_12/settings.py
@@ -160,6 +160,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # You can use this directory to store project-wide static files.
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    BASE_DIR / 'daftarDonasi/static',
 ]
 
 # Make sure the directories exist to prevent errors when doing `collectstatic`.
